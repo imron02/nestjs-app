b9f4fada3923e0617533750eb89b751f538765d4
Imron Rosdiana <imron02@gmail.com>
2022-10-23 01:09:04 +0700

    finalize nest jwt auth example

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,5 @@
-import {
-  Body,
-  Controller,
-  HttpException,
-  HttpStatus,
-  Post,
-} from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import { LoginDto } from 'src/dto/auth.dto';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from 'src/guards/local-auth.guard';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 
@@ -17,21 +10,9 @@ export class AuthController {
     private userService: UserService,
   ) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Body() loginDto: LoginDto): Promise<any> {
-    const user = await this.userService.findByEmail(loginDto.email);
-
-    if (user) {
-      const isPasswordValid = bcrypt.compareSync(
-        loginDto.password,
-        user.password,
-      );
-
-      if (isPasswordValid) {
-        return this.authService.login(user);
-      }
-    }
-
-    throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+  async login(@Request() req): Promise<any> {
+    return this.authService.login(req.user);
   }
 }
